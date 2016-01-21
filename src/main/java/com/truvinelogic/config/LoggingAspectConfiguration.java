160c6831a00b89d586d12896d9aad2e34d9fef3a
saflimbenly <safaridin.salim@gmail.com>
2016-01-21 18:50:44 +0700

    Initial commit

---
+++ src/main/java/com/truvinelogic/config/LoggingAspectConfiguration.java
@@ -0,0 +1,15 @@
+package com.truvinelogic.config;
+
+import com.truvinelogic.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
---
+++ src/main/java/com/truvinelogic/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.truvinelogic.repository;
+
+import com.truvinelogic.domain.PersistentToken;
+import com.truvinelogic.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
